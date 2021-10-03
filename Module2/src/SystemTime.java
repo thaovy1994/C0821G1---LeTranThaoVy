d60c0bf9fe21ff77aa5081056090b20b401cb4d9
unknown <letranthaovy1326@gmail.com>
2021-10-03 17:12:44 +0700

    mina

--- Module2/src/SystemTime.java
+++
@@ -1,8 +0,0 @@
-import java.util.Date;
-public class SystemTime {
-    public static void main(String[] args) {
-        Date now = new Date(); // Lấy ra thời gian hiện tại
-
-        System.out.println("Now is: " + now);
-    }
-}
